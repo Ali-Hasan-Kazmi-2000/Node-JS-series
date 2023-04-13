a631f926fb8f6a4024d850d841c10d12cb836bf2
ALI HASAN KAZMI <alihasan99.2@yahoo.com>
2023-04-13 23:22:46 +0500

    Add files via upload
    This is a slightly modified version of the previous code, with the main difference being that the fs.writeFileSync() method has been replaced with the fs.writeFile() method.
    
    The fs.writeFile() method is asynchronous and takes a callback function that is executed when the file has been written. This allows the server to respond to the client only after the file has been written, avoiding potential issues with blocking the event loop.
    
    Additionally, the callback function sets the response status code and location header inside the fs.writeFile() method, which ensures that the response is not sent until the file has been written.
    
    Overall, these changes make the server more robust and scalable, as it can handle multiple requests concurrently without blocking the event loop.

---
+++ DAY_3/blocking_and_non_blocking_Code.js
@@ -0,0 +1,54 @@
+const http = require("http");
+const fs = require("fs");
+
+const PORT = 3000;
+
+const server = http.createServer((req, res) => {
+  const url = req.url;
+  const method = req.method;
+
+  if (url === "/") {
+    res.write("<html>");
+    res.write("<head>");
+    res.write("<title>Input Form Message</title>");
+    res.write("</head>");
+    res.write("<body>");
+    res.write("<form action='/message'  method='POST'>");
+    res.write("<input type='text' name='message'/>");
+    res.write("<button type='submit' >Send</button>");
+    res.write("</form>");
+    res.write("</body>");
+    res.write("</html>");
+    return res.end();
+  }
+
+  if (url === "/message" && method == "POST") {
+    const body = [];
+    req.on("data", (chunk) => {
+      body.push(chunk);
+    });
+    return req.on("close", () => {
+      const parsedBody = Buffer.concat(body).toString();
+      const message = parsedBody.split("=")[1];
+      fs.writeFile("message.txt", message.replaceAll("+", " "), (err) => {
+        res.statusCode = 302;
+        res.setHeader("location", "/");
+        return res.end();
+      });
+    });
+  }
+
+  res.setHeader("Content-Type", "text/html");
+
+  res.write("<html>");
+  res.write("<head>");
+  res.write("<title>Node JS</title>");
+  res.write("</head>");
+  res.write("<body>");
+  res.write("<h1>Node Js Development</h1>");
+  res.write("</body>");
+  res.write("</html>");
+  res.end();
+});
+
+server.listen(PORT);
